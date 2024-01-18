687b866d5ffddfd4f8fc2c6c5d0698d0dac14293
Cl1493 <152617155+Cl1493@users.noreply.github.com>
2024-01-18 13:26:45 +0000

    Commit inicial

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var excuse = document.getElementById("excuse");
+  excuse.innerHTML = GeneraExcusas(who, action, what, when);
 };
+
+let who = ["El perro", "Mi abuela", "La tortuga", "El pájaro", "Mi hijo"];
+let action = ["se comió", "se hizo pipi", "rompió", "ahogó", "tiró"];
+let what = ["mis deberes", "las llaves", "el coche", "la casa", "el trabajo"];
+let when = [
+  "antes de clase",
+  "justo ahora",
+  "cuando terminé",
+  "durante la comida",
+  "mientras estaba rezando"
+];
+
+function GeneraExcusas(who, action, what, when) {
+  let RandomWho = Math.floor(Math.random() * who.length);
+  let RandomAction = Math.floor(Math.random() * action.length);
+  let RandomWhat = Math.floor(Math.random() * what.length);
+  let RandomWhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[RandomWho] +
+    " " +
+    action[RandomWho] +
+    " " +
+    what[RandomWhat] +
+    " " +
+    when[RandomWhen]
+  );
+}
+git;
